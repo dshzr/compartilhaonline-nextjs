0483e914ebe29a7c930743e2678398648bf57900
Wellington <wellingtonhmt2011@gmail.com>
2025-02-19 18:56:56 -0300

    vercel

--- db.ts
+++ db.ts
@@ -7,14 +7,8 @@ const db = knex({
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   },
   pool: {
-    min: 2,
-    max: 10,
-    createTimeoutMillis: 3000,
-    acquireTimeoutMillis: 30000,
-    idleTimeoutMillis: 30000,
-    reapIntervalMillis: 1000,
-    createRetryIntervalMillis: 100,
-    propagateCreateError: false
+    min: 1,
+    max: 5,
   }
 });
 
