e3d04b8362df72fa5d9d4629f296103271d1f441
compartilhaonline <onlinecompartilha@gmail.com>
2025-02-19 22:03:57 -0300

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  images: {
-    domains: ['localhost'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
-    ],
-  },
-  webpack: (config) => {
-    // Ignora os dialetos do Knex que não estamos usando
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'mssql': false,
-      'mysql': false,
-      'mysql2': false,
-      'oracledb': false,
-      'sqlite3': false,
-      'tedious': false
-    }
-
-    // Adiciona módulos externos
-    config.externals = [
-      ...(config.externals || []),
-      'pg-query-stream',
-      'pg-native'
-    ];
-
-    return config;
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
